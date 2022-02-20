b3f0f4973c2474089f815244a0fa1e9a5a7bc69c
François Nicolas <francois.prm.nicolas@gmail.com>
2022-02-20 11:49:19 +0100

    test_floyd and test_brent perfect.

--- tortoise_and_hare.py
+++ tortoise_and_hare.py
@@ -1,55 +1,38 @@
-def floyd(f, x1):
-    p = 1
-    tortoise = x1 
-    hare = f(x1)
+def floyd(f, tortoise):
+    step = 0
+    hare = f(tortoise)
     while tortoise != hare:
-        p += 1
+        step += 1
         tortoise = f(tortoise)
         hare = f(f(hare))   # The hare moves twice as fast as the tortoise
-    return tortoise, p
+    return tortoise, step + 1
 
 
-def brent(f, x1):
-    q = p = 1
-    hare = x1
-    tortoise = f(x1)
+def brent(f, hare):
+    p = 0
+    q = 1
+    tortoise = f(hare)
     while tortoise != hare:
         if p == q:  # time to start a new power of two?
             hare = tortoise  # The hare jumps !!
-            q <<= 1
             p = 0
+            q <<= 1
         tortoise = f(tortoise)
         p += 1
-    return hare, q, p
+    return hare, p, q
 
 
-def eventually_periodic(q, p):
-    def f(x):
-        x += 1
-        if x == p + q: return q
-        return x 
-    return f
+def period(f, x):
+    l = [x]
+    y = f(x)
+    while x != y:
+        l.append(y)
+        y = f(y)
+    return l
 
 
-def test_floyd(n):
-    for q in range(n):
-        x, p_floyd = floyd(eventually_periodic(q, n - q), 0)
-        assert x >= q
-        assert p_floyd > q
-        assert p_floyd % (n - q) == 0
-
-
-def test_brent(n):
-    for q in range(n):
-        x, q_brent, p_brent = brent(eventually_periodic(q, n - q), 0)
-        assert x >= q
-        assert q_brent > q
-        assert q_brent >= n - q
-        assert q_brent <= 2 * max(q, n - q - 1)
-        assert p_brent == n - q
-
-        
 class count_calls:
+    
     def __init__(self, f):
         self.func = f
         self.calls = 0
@@ -59,23 +42,44 @@ class count_calls:
         return self.func(x)
 
     
+def loop_inc(p):
+    return lambda x: x + 1 if x <= p - 2 else 0
+
+
+def test_floyd(per, pre):
+    f = count_calls(loop_inc(per))
+    x, p = floyd(f, - pre)
+    assert per - 1 - pre % per == x
+    assert per + pre - pre % per == p
+    assert f.calls == 3 * (p - 1) + 1
+    
+
+def test_brent(per, pre):
+    f = count_calls(loop_inc(per))
+    x, p, q = brent(f, - pre)
+    assert (q - pre) % per == x
+    assert per == p
+    assert max(per, pre) <= q <= 2 * max(per, pre) - 2
+    assert f.calls == p + q 
+    
+
 def compare_floyd_and_brent():
 
-    powersof2 = [2 ** k for k in range(1, 11)]
+    powersof2 = [2 ** k for k in range(1, 14)]
     
     print()
     print("Purely periodic case.")
     print()
 
-    print("Period  Floyd  Brent" )
+    print("Period  Floyd == Brent" )
     for p in powersof2:
-        f = count_calls(eventually_periodic(0, p + 1))
-        _, _, p_brent = brent(f, 0)
+        f = count_calls(loop_inc(p + 1))
+        _, p_brent, _ = brent(f, 0)
         nb_brent = f.calls 
         f.calls = 0
-        _, p_floyd = floyd(f, 0)
-        assert p_brent == p_floyd
-        print(f"{p_brent:6d}{f.calls:7}{nb_brent:7}")
+        floyd(f, 0)
+        assert nb_brent == f.calls
+        print(f"{p_brent:6d}{f.calls:16}")
 
     print()
     print("Eventually constant case.")
@@ -84,17 +88,18 @@ def compare_floyd_and_brent():
     f = count_calls(lambda x: x - 1 if x > 0 else 0)
     print("Preperiod  Floyd  Brent  Ratio") 
     for n in powersof2: 
-        _, q_brent, _ = brent(f, n - 1)
-        assert q_brent == n
+        brent(f, n)
         nb_brent = f.calls
         f.calls = 0
-        _, p_floyd = floyd(f, n - 1)
-        assert p_floyd == n
+        floyd(f, n)
         nb_floyd = f.calls
         f.calls = 0
-        print(f"{n - 1:9}{nb_floyd:7}{nb_brent:7}{nb_floyd/nb_brent:7.3f}")
+        print(f"{n:9}{nb_floyd:7}{nb_brent:7}{nb_floyd/nb_brent:8.4f}")
+    
+        
+n = 300
+for q in range(n):
+    test_floyd(n - q, q)
+    test_brent(n - q, q)
 
-
-def compare_floyd_and_brent():
-test_floyd(2000)
-test_brent(2000)
+compare_floyd_and_brent()
